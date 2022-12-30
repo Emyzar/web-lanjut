f1d4b9b2c77438602c31169e8d67991cc3588720
Younixue3 <bussiness.needs.rctka@gmail.com>
2022-12-30 16:21:54 +0800

    Fixing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     public function form_user() {
         $lomba = lomba::all();
         return view('user.form_user', compact('lomba'));
-        
+
     }
 
     public function form_update($id) {
@@ -28,18 +28,18 @@ class UserController extends Controller
     }
     public function user_simpan(ProjectRequest  $request) {
         $user = new User();
-        $user->name = $request->get("nama");
+        $user->name = $request->get("name");
         $user->username = $request->get("username");
         $user->email = $request->get("email");
         $user->password = $request->get("password");
         $user->level_users= $request->get("level");
         $user->lomba_id = $request->get("lomba_id");
         $user->save();
-        
+
         return redirect(route("tampil-user")); // redirect: mengarahkan kalimat URL tertentu.
     }
 
-    public function hapus($id) 
+    public function hapus($id)
     {
         user::destroy($id); //kode untuk menghapus data user berdasarkan id-nya
         return redirect(route("user_all")); //redirect: mengarahkan kealamat URL tertentu.
@@ -52,8 +52,8 @@ class UserController extends Controller
         $user->password = $request->get("password");
         $user->level= $request->get("level");
         $user->save();
-        
-        return redirect(route("tampil-user")); // redirect: mengarahkan kalimat URL tertentu.  
+
+        return redirect(route("tampil-user")); // redirect: mengarahkan kalimat URL tertentu.
     }
 
     public function form_input(ProjectRequest $request){
@@ -64,12 +64,12 @@ class UserController extends Controller
         $user->password = $request->get("password");
         $user->level= $request->get("level");
         $user->save();
-        
-        return redirect(route("tampil-user")); // redirect: mengarahkan kalimat URL tertentu. 
+
+        return redirect(route("tampil-user")); // redirect: mengarahkan kalimat URL tertentu.
 
     }
 
-    public function show($id) 
+    public function show($id)
     {
         $data_user = User::find($id); //ambil data pada tabel user berdasarkan id
         return view("user.show")->with("data_user", $data_user); // tampilkan menggunakan view
